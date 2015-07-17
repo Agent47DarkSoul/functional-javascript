e383d34ff958e35a4b2e4d7c02acef5f4c55c3ba
Danish Satkut <danish.satkut@infibeam.net>
2015-07-17 16:33:00 +0530

    Refactor tail recursive version to move the looping function inside the main function.

--- common.js
+++ common.js
@@ -1,10 +1,10 @@
-function fact (number, accumulator) {
-  if (number == 0) { return accumulator; }
-  else { return fact(number - 1, number * accumulator); }
-}
-
 function factorial(n) {
-  return fact(n, 1);
+  function loop (number, accumulator) {
+    if (number == 0) { return accumulator; }
+    else { return loop(number - 1, number * accumulator); }
+  }
+
+  return loop(n, 1);
 }
 
 exports.factorial = factorial;
