d6a4f6d6e1a92a03b41ad5803b5cbdfeb0f04b32
Danish Satkut <danish.satkut@infibeam.net>
2015-07-17 16:29:38 +0530

    Convert factorial to tail-recursive.

--- common.js
+++ common.js
@@ -1,6 +1,10 @@
+function fact (number, accumulator) {
+  if (number == 0) { return accumulator; }
+  else { return fact(number - 1, number * accumulator); }
+}
+
 function factorial(n) {
-  if (n == 0) { return 1; }
-  else { return n * factorial(n - 1); }
+  return fact(n, 1);
 }
 
 exports.factorial = factorial;
