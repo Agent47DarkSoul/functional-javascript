08cd5aeca5152c212932afc7dff8dd34c617a78c
Danish Satkut <danish.satkut@infibeam.net>
2015-07-17 17:10:31 +0530

    Refactor sums to use currying.

--- sums.js
+++ sums.js
@@ -1,26 +1,31 @@
 var assert = require('assert');
 var common = require('./common');
 
-function sumOfOperation (from, to, operation) {
-  function loop(current, accumulator) {
-    if (current > to) { return accumulator; }
-    else {
-      // This changes for different problems
-      var accumulation = accumulator + operation(current);
+function sum (operation) {
+  return function(from, to) {
+    function loop(current, accumulator) {
+      if (current > to) { return accumulator; }
+      else {
+        // This changes for different problems
+        var accumulation = accumulator + operation(current);
 
-      // This will always be like this
-      return loop(current + 1, accumulation)
+        // This will always be like this
+        return loop(current + 1, accumulation)
+      }
     }
-  }
 
-  return loop(from, 0);
+    return loop(from, 0);
+  }
 }
 
-var sumOfNumbers = sumOfOperation(1, 3, function (n) { return n; });
-assert(sumOfNumbers === 6, 'Sum of numbers failed: Expected 6, Got ' + sumOfNumbers);
+var sumOfNumbers = sum(function (n) { return n; });
+var numbersResult = sumOfNumbers(1, 3);
+assert(numbersResult === 6, 'Sum of numbers failed: Expected 6, Got ' + numbersResult);
 
-var sumOfCubes = sumOfOperation(1, 3, function (n) { return n * n * n; });
-assert(sumOfCubes === 36, 'Sum of cubes failed: Expected 36, Got ' + sumOfCubes);
+var sumOfCubes = sum(function (n) { return n * n * n; });
+var cubesResult = sumOfCubes(1, 3);
+assert(cubesResult === 36, 'Sum of cubes failed: Expected 36, Got ' + cubesResult);
 
-var sumOfFactorials = sumOfOperation(1, 3, common.factorial);
-assert(sumOfFactorials === 9, 'Sum of factorials failed: Expected 9, Got ' + sumOfFactorials);
+var sumOfFactorials = sum(common.factorial);
+var factorialsResult = sumOfFactorials(1, 3);
+assert(factorialsResult === 9, 'Sum of factorials failed: Expected 9, Got ' + factorialsResult);
