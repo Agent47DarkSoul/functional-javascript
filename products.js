e714a47c6c4af1d999e7b20ad9477ba68fd2b7b0
Danish Satkut <danish.satkut@infibeam.net>
2015-07-17 18:00:17 +0530

    Create factorial using product.

--- products.js
+++ products.js
@@ -22,3 +22,11 @@ assert(sumOfCubes === 216, 'Product of cubes failed: ' + sumOfIntegers);
 
 var sumOfFactorials = product(function (n) { return common.factorial(n); })(1, 3);
 assert(sumOfFactorials === 12, 'Products of factorials failed: ' + sumOfFactorials);
+
+function advancedFactorial (number) {
+  var basicProduct = product(function (n) { return n; });
+  return basicProduct(1, number);
+}
+
+var factorialResult = advancedFactorial(5);
+assert(factorialResult === 120, 'Factorial using product failed: ' + factorialResult);
