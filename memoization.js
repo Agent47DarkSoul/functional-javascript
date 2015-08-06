f8903eaad717c694ea336c6f8b61e61d48c3a986
Danish Satkut <danish.satkut@infibeam.net>
2015-08-06 10:18:58 +0530

    Add memoization to fibonacci.

--- memoization.js
+++ memoization.js
@@ -1,11 +1,19 @@
 var assert = require('assert');
 
-function fibonacci(number) {
-  if (number < 2) { return number; }
-  else {
-    return fibonacci(number - 1) + fibonacci(number - 2);
-  }
-}
+var fibonacci = (function () {
+  var _mem = [];
+
+  return function (number) {
+    if (number < 2) { return number; }
+    else {
+      if (_mem[number] === undefined) {
+        _mem[number] = fibonacci(number - 1) + fibonacci(number - 2);
+      }
+
+      return _mem[number];
+    }
+  };
+})();
 
 var answer = fibonacci(20);
 assert(answer === 6765, 'Fibonacci failed: ' + answer);
