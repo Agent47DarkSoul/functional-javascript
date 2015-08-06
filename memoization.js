4097f6f788d289a17b05bbbdc729e868df52ddc5
Danish Satkut <danish.satkut@infibeam.net>
2015-08-06 11:08:24 +0530

    Completed memoization.

--- memoization.js
+++ memoization.js
@@ -1,39 +1,51 @@
 var assert = require('assert');
 
-var fibonacci = (function () {
-  var _mem = [];
+Function.prototype.memoizer = (function() {
+  // this => the actual function we want to memoize
 
-  return function (number) {
-    if (number < 2) { return number; }
-    else {
-      if (_mem[number] === undefined) {
-        _mem[number] = fibonacci(number - 1) + fibonacci(number - 2);
+  return (function () {
+    var _mem = [];
+
+    return function (fn, args) {
+      if (_mem[args[0]] === undefined) {
+        // console.log('Generating ', fn.name, args[0]);
+        _mem[args[0]] = fn.apply(fn, args);
       }
 
-      return _mem[number];
-    }
-  };
-})();
+      return _mem[args[0]];
+    };
+  })();
+});
 
-var factorial = (function () {
-  var _mem = [];
+Function.prototype.memoize = function() {
+  var fn = this;
+  var memoized = fn.memoizer();
 
-  return function (number) {
-    if(number === 0) { return 1; }
-    else {
-      if (_mem[number] === undefined) {
-        console.log('Calculating for: ', number);
-        _mem[number] = number * factorial(number - 1);
-      }
+  return function () {
+    return memoized(fn, arguments);
+  }
+};
 
-      return _mem[number];
-    }
-  };
-})();
+var fibonacci = (function (number) {
+  if (number < 2) { return number; }
+  else {
+    return fibonacci(number - 1) + fibonacci(number - 2);
+  }
+}).memoize();
+
+var factorial = (function (number) {
+  if(number === 0) { return 1; }
+  else { return number * factorial(number - 1); }
+}).memoize();
 
 (function () {
   var answer = fibonacci(20);
   assert(answer === 6765, 'Fibonacci failed: ' + answer);
+})();
+
+(function () {
+  var answer = fibonacci(22);
+  assert(answer === 17711, 'Fibonacci failed: ' + answer);
 })();
 
 
