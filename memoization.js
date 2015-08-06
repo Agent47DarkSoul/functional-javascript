2f421bf00214cdfc6ed3a818c25e8f3365787250
Danish Satkut <danish.satkut@infibeam.net>
2015-08-06 10:28:55 +0530

    Add factorial function.

--- memoization.js
+++ memoization.js
@@ -15,5 +15,35 @@ var fibonacci = (function () {
   };
 })();
 
-var answer = fibonacci(20);
-assert(answer === 6765, 'Fibonacci failed: ' + answer);
+var factorial = (function () {
+  var _mem = [];
+
+  return function (number) {
+    if(number === 0) { return 1; }
+    else {
+      if (_mem[number] === undefined) {
+        console.log('Calculating for: ', number);
+        _mem[number] = number * factorial(number - 1);
+      }
+
+      return _mem[number];
+    }
+  };
+})();
+
+(function () {
+  var answer = fibonacci(20);
+  assert(answer === 6765, 'Fibonacci failed: ' + answer);
+})();
+
+
+(function () {
+  var answer = factorial(5);
+  assert(answer === 120, 'Factorial failed: ' + answer);
+})();
+
+
+(function () {
+  var answer = factorial(10);
+  assert(answer === 3628800, 'Factorial failed: ' + answer);
+})();
